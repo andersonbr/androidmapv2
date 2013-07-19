f414fd9e23ec4c0969eb75f49f20cb80132f7b4f
Anderson Calixto <andersonbr@gmail.com>
2013-07-19 13:02:10 -0300

    second map activity ok!

--- src/br/com/shellcode/maps/v2/MainActivity.java
+++ src/br/com/shellcode/maps/v2/MainActivity.java
@@ -1,15 +1,38 @@
 package br.com.shellcode.maps.v2;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	private static String TAG = "Maps";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Button botao = (Button) findViewById(R.id.button1);
+		botao .setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				Log.i(TAG, "clique!");
+				// vibrator.vibrate(2000);
+				sendMessage(view);
+			}
+
+		});
+	}
+	
+	/** Called when the user clicks the Send button */
+	public void sendMessage(View view) {
+		Intent intent = new Intent(this, MapAddressSearchActivity.class);
+		startActivity(intent);
 	}
 
 	@Override
